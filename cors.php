24f7e0333a906f56f4b8007fdb1f8cb97350c3b2
Christoph Honal <christoph.honal@web.de>
2020-10-26 01:42:23 +0100

    fix

--- cors.php
+++ cors.php
@@ -2,7 +2,7 @@
 
     if(isset($_GET["url"]))
     {
-        if (strpos($string2, 'https://api.steampowered.com') === 0) // prevent proxy abuse
+        if (strpos(urldecode($_GET["url"]), 'https://api.steampowered.com') === 0) // prevent proxy abuse
         {
             $content = file_get_contents(urldecode($_GET['url'])); 
             header('Access-Control-Allow-Origin: *'); 
